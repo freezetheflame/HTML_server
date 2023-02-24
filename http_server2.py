54f026090fab2a326a9459898feb90971686750f
Scotchingice <1439921564@qq.com>
2023-02-24 23:13:06 +0800

    second server

---
+++ http_server2.py
@@ -0,0 +1,12 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class MyRequestHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        f=open("hello.html","rb")
+        self.wfile.write(f.read())
+
+httpd = HTTPServer(('localhost', 8000), MyRequestHandler)
+httpd.serve_forever()
